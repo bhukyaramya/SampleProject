9b97bac0b718b7fb404cbb68406ba199a5e6fcb2
Ramya Bhukya <Ramya Bhukya@LAPTOP-PG58VPJS.mshome.net>
2024-06-29 15:10:35 +0530

    first push

---
+++ SeleniumProject/src/test/java/pack3/CreateEmailTest.java
@@ -0,0 +1,16 @@
+package pack3;
+
+import org.testng.annotations.Test;
+
+public class CreateEmailTest {
+
+	@Test
+	public void createEmail() {
+		System.out.println("email is created");
+	}
+	@Test
+	public void modifyEmail() {
+		System.out.println("email is modifyEmail");
+	}
+	
+}
